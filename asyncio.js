bd715587addf3bbfb3f696a3046edab2eb51d717
tkling <tyler.kling@careerbuilder.com>
2017-01-19 16:07:14 -0500

    asynchonous i/o

---
+++ asyncio.js
@@ -0,0 +1,8 @@
+var fs = require('fs')
+
+function doneReading(err, fileContents) {
+  if (err) return console.error(err);
+  console.log(fileContents.split("\n").length - 1)
+}
+
+fs.readFile(process.argv[2], 'utf8', doneReading)
