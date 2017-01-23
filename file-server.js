597498cf07e156b3c0f669aa0f42fede987374f6
tkling <tyler.kling@careerbuilder.com>
2017-01-23 10:02:10 -0500

    file server

---
+++ file-server.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var http = require('http');
+
+var port = process.argv[2];
+var file = process.argv[3];
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+  fs.createReadStream(file, {encoding: 'utf8'}).pipe(res);
+});
+
+server.listen(port);
