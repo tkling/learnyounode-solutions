d510d0ee0882c47d5105951d643d0a1b90863878
tkling <tyler.kling@careerbuilder.com>
2017-01-23 10:10:31 -0500

    upcase server

---
+++ upcase-server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var map = require('through2-map');
+
+var server = http.createServer(function(req, res) {
+  if (req.method !== 'POST') {
+    res.writeHead(400, { 'content-type': 'text/plain' });
+    return res.end('POST PLZ!\n')
+  }
+
+  req.pipe(map(function(chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(res);
+});
+
+server.listen(process.argv[2]);
